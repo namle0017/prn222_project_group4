b37e82db10a72bbc24f603bfbf7b0419ca25685b
Pham Hong Phuc <hongphuc.pham.7777@gmail.com>
2026-06-08 14:23:21 +0700

    feat: add shared select option dto

---
+++ Models/Dtos/SharedDtos/SelectOptionDto.cs
@@ -0,0 +1,9 @@
+namespace FapWeb.Models.Dtos.SharedDtos
+{
+    public class SelectOptionDto
+    {
+        public string Value { get; set; } = string.Empty;
+
+        public string Label { get; set; } = string.Empty;
+    }
+}
